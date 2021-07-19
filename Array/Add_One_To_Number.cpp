ab1ff44f71b62c84e058bbe6f3adfa642893192f
Mohammed Aadil <66634743+XXDIL@users.noreply.github.com>
2021-07-19 14:59:46 +0400

    Update Add_One_To_Number.cpp

--- Array/Add_One_To_Number.cpp
+++ Array/Add_One_To_Number.cpp
@@ -1,28 +1,24 @@
 vector<int> Solution::plusOne(vector<int>& A) 
 {   
-    // removing leading 0's : [0, 0, 1, 2, 3] --> [1, 2, 3]
-    auto it = A.begin();
-    while(*it == 0 && A.size() > 1) A.erase(it);
-    
     int n = A.size();
-    bool carry = 1; // assume carry is there
-    
-    // tackeling the chain of carries
-    int i = n-1;
-    while(carry == 1 && i>=0)
+
+    // removing leading 0's
+    int i = 0;
+    while(i<n-1 && A[i] == 0) i++;
+    A = vector<int>(A.begin()+i, A.end());
+
+    int carry = 1;
+    n = A.size();
+    for(int i=n-1;i>=0;--i)
     {
-        A[i] += 1;
-        if(A[i] == 10)
-        {
-            carry = 1;
-            A[i] = 0;
-        }
-        else carry = 0;
-        --i;
+        int sum = A[i]+carry;
+
+        A[i] = sum%10;
+        carry = sum/10;
     }
-    
-    // last carry needs a new index
-    if(carry == 1) A.insert(A.begin(), 1);
+
+    if(carry)
+        A.insert(A.begin(), 1);
     
     return A;
 }
